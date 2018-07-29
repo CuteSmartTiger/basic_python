eb64a2ea687cc7356cb6e2b7df72852363d1d6bc
liuhu <max_liuhu@163.com>
2018-07-29 20:14:34 +0800

    add more

--- operations of str and text/str_text.py
+++ operations of str and text/str_text.py
@@ -5,7 +5,12 @@
 # 4.分割
 # 5.取值
 # 6.增加
-# 7.疑问清单：正则中?:的用法;match与search的区别；如何将文件中各种语言转化为同一标准，然后输出不乱码
+# 7.对齐文本字符串
+#8.字符串连接与合并
+#9.以重新的列数重新格式化文本
+# 疑问清单：
+# 知识点：正则中?:的用法;match与search的区别；如何将文件中各种语言转化为同一标准，然后输出不乱码
+# 提高效率的点：快速回到当前的顶部或者底部 ；删除整行
 
 
 import re
@@ -148,3 +153,102 @@ bytes_tabtrans = bytes.maketrans(b'abcdefghijklmnopqrstuvwxyz', b'ABCDEFGHIJKLMN
 
 # 转换为大写，并删除字母o
 print(b'runoob'.translate(bytes_tabtrans, b'o'))
+
+
+
+# 对齐文本字符串
+# 方法一：对于基本的字符串对齐操作，可以使用字符串的 ljust() , rjust() 和 center() 方法
+
+text = 'Hello World'
+print(text.ljust(20))
+# 'Hello World         '
+print(text.rjust(20))
+# '         Hello World'
+print(text.center(20))
+# '    Hello World     '
+
+
+# 所有这些方法都能接受一个可选的填充字符
+print(text.rjust(20,'='))
+# '=========Hello World'
+print(text.center(20,'*'))
+# '****Hello World*****'
+
+# 方法二：函数 format() 同样可以用来很容易的对齐字符串
+# 使用 <,> 或者 ^ 字符后面紧跟一个指定的宽度,可理解为显示区域的宽度
+print(format(text, '>20'))
+# '         Hello World'
+print(format(text, '<20'))
+# 'Hello World         '
+print(format(text, '^40'))
+'    Hello World     '
+
+# 当格式化多个值的时候，这些格式代码也可以被用在 format() 方法中。
+print('{:>10s} {:>10s}'.format('Hello', 'World'))
+# '     Hello      World'
+
+x = 1.2345
+print(format(x, '>10'))
+# '    1.2345'
+print(format(x, '^10.2f'))
+
+
+# 字符串连接与合并
+# 方法有：join  format   +  三种
+# 使用迭代器进行字符串的连接，减少内存的消耗
+data = ['ACME', 50, 91.1]
+print(','.join(str(d) for d in data))
+
+print('a' + ':' + 'b' + ':' + 'c') # Ugly
+print(':'.join(['a', 'b', 'c'])) # Still ugly
+print('a', 'b', 'c', sep=':') # Better
+
+# 注意三种用法的区分，不同的场景选择不同的方法
+# 当与I/O操作时
+# chunk1=""
+# chunk2=""
+# Version 1 (string concatenation)
+# f.write(chunk1 + chunk2)
+
+# Version 2 (separate I/O operations)
+# f.write(chunk1)
+# f.write(chunk2)
+# 如果两个字符串很小，那么第一个版本性能会更好些，
+# 因为I/O系统调用天生就慢。 另外一方面，如果两个
+# 字符串很大，那么第二个版本可能会更加高效
+
+# 如果你准备编写构建大量小字符串的输出代码,
+# 考虑下使用生成器函数，利用yield语句产生输出片段
+
+def sample():
+    yield 'Is'
+    yield 'Chicago'
+    yield 'Not'
+    yield 'Chicago?'
+# 这种方法一个有趣的方面是它并没有对输出片段到底要怎
+# 样组织做出假设。 例如，你可以简单的使用 join() 方法将这些片段合并起来：
+
+text8 = ''.join(sample())
+print(text8)
+# 或者你也可以将字符串片段重定向到I/O：
+
+# for part in sample():
+#     f.write(part)
+# 再或者你还可以写出一些结合I/O操作的混合方案：
+
+def combine(source, maxsize):
+    parts = []
+    size = 0
+    for part in source:
+        parts.append(part)
+        size += len(part)
+        if size > maxsize:
+            yield ':'.join(parts)
+            parts = []
+            size = 0
+    yield ' '.join(parts)
+
+# 结合文件操作
+with open('text.py', 'w') as f:
+    for part in combine(sample(), 32768):
+        f.write(part)
