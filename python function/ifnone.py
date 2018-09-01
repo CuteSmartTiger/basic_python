5438c4ce5befdcbfdc6a95156da0c4389cc4cc22
liuhu <max_liuhu@163.com>
2018-09-01 19:20:35 +0800

    if

---
+++ python function/ifnone.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: liuhu
+@license: (C) Copyright 2013-2018, Node Supply Chain Manager Corporation Limited.
+@contact: max_liuhu@163.com
+@software: pycharm
+@file: ifnone.py
+@time: 2018/8/27 9:53
+@desc:
+'''
+a = []
+if a :
+ print 'ok'
