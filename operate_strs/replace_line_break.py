afe0237c18d68cde16c34fee9cd3f45a29bbe0ad
MAX-liuhu <max_liuhu@163.com>
2018-11-30 14:20:09 +0800

    Create replace_line_break.py

---
+++ operate_strs/replace_line_break.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/30 14:06
+# @Author  : liuhu
+# @File    : replace_line_break.py
+# @Software: PyCharm
+# @github  :https://github.com/Max-Liuhu
+
+with open(r'test.txt','r') as f:
+    # content = f.readlines()
+    content = f.read()
+    print content
+    if r' ' in content:
+        pass
