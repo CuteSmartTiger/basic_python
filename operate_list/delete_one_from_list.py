45fbc46313e926de4bcad94e368a6a0da43fa7d4
MAX-liuhu <max_liuhu@163.com>
2019-01-23 19:33:48 +0800

    Create delete_one_from_list.py

---
+++ operate_list/delete_one_from_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/14 19:12
+# @Author  : liuhu
+# @File    : delete_one_from_list.py
+# @Software: PyCharm
+# @github  :https://github.com/Max-Liuhu
+lista = [1,2,3,4]
+lista.remove(2)
+print lista
