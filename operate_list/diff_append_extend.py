715ff8bc054ae566331284b850ec106d7e7b45c3
MAX-liuhu <max_liuhu@163.com>
2019-01-23 19:33:55 +0800

    Create diff_append_extend.py

---
+++ operate_list/diff_append_extend.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/8 16:20
+# @Author  : liuhu
+# @File    : diff_append_extend.py
+# @Software: PyCharm
+# @github  :https://github.com/Max-Liuhu
+
+
+hu = [1,2,3]
+hi = ['8','7',4]
+# hu.extend(hi)
+print hu.append(hi)
+print hu
