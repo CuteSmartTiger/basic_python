8d4c6e569016aaa0e80c5de9e41344402b556e5d
MAX-liuhu <max_liuhu@163.com>
2019-01-23 19:34:06 +0800

    Create insert_auto.py

---
+++ operate_mysql/insert_auto.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/9 17:22
+# @Author  : liuhu
+# @File    : insert_auto.py
+# @Software: PyCharm
+# @github  :https://github.com/Max-Liuhu
+# from sqlalchemy import create_engine,Column,Integer,String,DateTime
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+# HOSTNAME = '192.168.6.91'
+# PORT = '3306'
+# DATABASE = 'desktop'
+# USERNAME = 'root'
+# PASSWORD = '123123'
+#
+# # dialect+driver://username:password@host:port/database
+# DB_URI = "mysql+pymysql://{username}:{password}@{host}:{port}/{db}?charset=utf8".format(username=USERNAME,password=PASSWORD,host=HOSTNAME,port=PORT,db=DATABASE)
+#
+# engine = create_engine(DB_URI)
+
+
+
+
+
+
+
+
+
+
+
