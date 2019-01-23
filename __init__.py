9cef62922b3e4cb89a64d11b527d75d2a379bd97
MAX-liuhu <max_liuhu@163.com>
2019-01-23 19:33:33 +0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/23 17:37
+# @Author  : liuhu
+# @File    : __init__.py.py
+# @Software: PyCharm
+# @github  :https://github.com/Max-Liuhu
