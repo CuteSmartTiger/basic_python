7972041dbe213200288736bea97513f17d1b23c4
MAX-liuhu <max_liuhu@163.com>
2019-01-23 19:34:19 +0800

    Create test_dict.py

---
+++ operate_dict/test_dict.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/7 11:46
+# @Author  : liuhu
+# @File    : test_dict.py
+# @Software: PyCharm
+# @github  :https://github.com/Max-Liuhu
+
+dict = {1:'jishu'}
+print dict[1]
