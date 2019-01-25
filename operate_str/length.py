52000971bb5e1a966f622992a463452fa124457f
MAX-liuhu <max_liuhu@163.com>
2019-01-25 17:59:07 +0800

    Create length.py

---
+++ operate_str/length.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/25 9:58
+# @Author  : liuhu
+# @File    : length.py
+# @Software: PyCharm
+# @github  :https://github.com/Max-Liuhu
+print len(str(123123))
+print type(u'类型')
+print len(u'类型')
+
