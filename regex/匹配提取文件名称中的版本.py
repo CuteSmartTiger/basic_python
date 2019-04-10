bdc4c947895ccb66daa62cb61c5f8de78219aa2b
MAX-liuhu <max_liuhu@163.com>
2019-04-10 20:29:21 +0800

    Update 匹配提取文件名称中的版本.py

--- regex/匹配提取文件名称中的版本.py
+++ regex/匹配提取文件名称中的版本.py
@@ -14,8 +14,6 @@ import re
 # re.match(pattern, string[, flags])
 
 
-
-
 # print re.split(r'-|_',filename)
 # res_list = re.split(r'[-|_]', filename)
 # print res_list
@@ -47,7 +45,78 @@ import re
 #
 #
 # print regx_version(filename)
-filename = 'VDIAgentInstaller_x64_5.1.1.1-2019011614'
-pattern = r'(.*)(([0-9]{1,4}\.){3}[0-9]{1,4})(.*)'
+filename = 'VDIAgentInstaller_x64_5.1.1.1.zip'
+# filename = '5.1.1.1'
+# pattern = r'(.*)(([0-9]{1,4}\.){3}[0-9]{1,4})?(.*)'
+pattern = r'(.*[-|_])?((([0-9]{1,4}\.){3}[0-9]{1,4}){1})([-|_].*[\.]{1}.*?)'
+# pattern = r'(.*[-|_])?((([0-9]{1,4}\.){3}[0-9]{1,4}){1})(.*)'
 response = re.match(pattern, filename)
-print response.group(2)
+
+
+# print response.group(1)
+# print response.group(2)
+# print response.group(3)
+# print response.group(4)
+# print response.group(0)
+
+
+def regex_version(filename):
+    '''正则提取版本号'''
+    pattern = r'(.*[-|_])?((([0-9]{1,4}\.){3}[0-9]{1,4}){1})([-|_].*)'
+    response = re.match(pattern, filename)
+    try:
+        version = response.group(2)
+        print version
+    except AttributeError as e:
+        return False, None
+    else:
+        return True, version
+
+
+print regex_version(filename)
+
+
+def check_suffix(filename):
+    suffix_list = [
+        'gz',
+        'bz2',
+        'zip',
+        'tar',
+        'tgz',
+        'txz',
+        '7z',
+        'rar',
+        'pdf',
+        'doc',
+        'docx',
+        'xlsx',
+        'xls',
+        'ppt',
+        'pptx',
+        'exe',
+        'EXE']
+    try:
+        version_split_list = filename.rsplit(".", 1)
+    except Exception as e:
+        return False
+    else:
+        if version_split_list[1] in suffix_list:
+            return True
+        else:
+            return False
+
+
+# print check_suffix(filename)
+
+# version = '5.1.1.1aa'
+# def check_verison(version):
+#     pattern = r'(([0-9]{1,4}\.){3}[0-9]{1,4}){1}$'
+#     response = re.match(pattern, version)
+#     try:
+#         version = response.group(1)
+#     except Exception as e:
+#         return False
+#     else:
+#         return True
+#
+# print check_verison(version)
