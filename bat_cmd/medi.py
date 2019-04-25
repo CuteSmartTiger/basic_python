5320fee64c925f92a53a2481dc036feb2fe6d2b2
MAX-liuhu <max_liuhu@163.com>
2019-04-25 19:32:40 +0800

    Create medi.py

---
+++ bat_cmd/medi.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/4/23 10:01
+# @Author  : liuhu
+# @File    : medi.py
+# @Software: PyCharm
+# @github  :https://github.com/Max-Liuhu
+# import subprocess
+# res = subprocess.Popen([r'D:\github\basic_python\bat_cmd\test.bat'])
+import os
+
+os.system(r'D:\github\basic_python\bat_cmd\test.bat')
+# print res.communicate()
